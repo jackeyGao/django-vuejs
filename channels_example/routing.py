d7084d672713634e73432a1052728d6ad366d21e
Jacob Kaplan-Moss <jacob@jacobian.org>
2016-03-01 14:49:33 -0800

    serve static files

--- channels_example/routing.py
+++ channels_example/routing.py
@@ -1,1 +1,8 @@
-channel_routing = {}
+from channels.staticfiles import StaticFilesConsumer
+
+channel_routing = {
+    # This makes Django serve static files from settings.STATIC_URL, similar
+    # to django.views.static.serve. This isn't ideal (not exactly production
+    # quality) but it works for a minimal example.
+    'http.request': StaticFilesConsumer()
+}
