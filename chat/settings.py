acd25afec049284224f0a426cd1029a80ad7bbc8
Jacob Kaplan-Moss <jacob@jacobian.org>
2016-03-04 10:02:50 -0800

    Read REDIS_URL from env

--- chat/settings.py
+++ chat/settings.py
@@ -104,7 +104,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "asgi_redis.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
         "ROUTING": "chat.routing.channel_routing",
     },
