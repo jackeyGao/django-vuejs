7bc69c8d095fc0286dbb022f3b78f3e966068dfb
Jacob Kaplan-Moss <jacob@jacobian.org>
2016-03-20 14:27:48 -0700

    Finished view tests.

--- chat/tests/test_views.py
+++ chat/tests/test_views.py
@@ -1,7 +1,7 @@
 # Use django.test.TestCase here so I get the snazzy Django assertions.
 
 from django.test import TestCase
-from chat.models import Room
+from chat.models import Room, Message
 
 class ViewTests(TestCase):
     def test_about(self):
@@ -12,3 +12,9 @@ class ViewTests(TestCase):
         r = self.client.get('/new/')
         room_label = r['Location'].strip('/')
         assert Room.objects.filter(label=room_label).exists()
+
+    def test_chat_room(self):
+        room = Room.objects.create(label='room1')
+        r = self.client.get('/room1/')
+        self.assertTemplateUsed(r, 'chat/room.html')
+        assert r.context['room'] == room
